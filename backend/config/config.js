79a60a4488f5140a02b4fe0c3dde33bfbbac46c1
khudaibergenkyzy.n <khudaibergenkyzy@gmail.com>
2022-09-07 14:00:26 +0600

    docker

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,12 +1,25 @@
-require('dotenv').config();
-
+// require('dotenv').config();
+console.log(process.env);
+// module.exports = {
+//   SECRET_KEY : 'secret1122',
+//   development: {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+//     define : {
+//       timestamps : true
+//     }
+//   }
+// }
 module.exports = {
   SECRET_KEY : 'secret1122',
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
     dialect: "postgres",
     define : {
       timestamps : true
